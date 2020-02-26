8e82e6b035cbc717dda63e892fae06810cedea49
Zhenkai Weng <oakrc@protonmail.com>
2020-02-25 21:48:48 -0800

    SQL fix

--- db/q.fl_sched.sql
+++ db/q.fl_sched.sql
@@ -4,10 +4,8 @@ SELECT
     rt_code AS fl_num,
     dt_dep,
     dt_arr,
-    b2u(a_id) AS ac_id,
-    fares.cap AS cap,
+    flights.cap AS cap,
     remain AS avail,
-    b2u(bin_af_id) AS af_id,
 FROM (
     SELECT
         f.id AS f_id,
@@ -15,21 +13,26 @@ FROM (
         r.code AS rt_code,
         f.dtime_depart AS dt_dep,
         f.dtime_arrive AS dt_arr,
-        f.aircraft_id AS a_id,
         (f.max_allowed - f.booked) AS remain,
+<<<<<<< Updated upstream
+||||||| merged common ancestors
+        ac.capacity AS cap,
+=======
+<<<<<<< HEAD
+        ac.capacity AS cap,
+||||||| merged common ancestors
         ac.capacity AS cap,
         af.id AS bin_af_id,
+=======
+>>>>>>> api
+>>>>>>> Stashed changes
     FROM
         flight_schedule AS f
     INNER JOIN routes AS r ON f.route_id=r.id
-    INNER JOIN airfares AS af ON af.flight_id=f.id
-    INNER JOIN aircrafts AS ac ON ac.id=f.aircraft_id
     WHERE
         DATE(f.dtime_depart) = DATE(?)
         AND r.src=?
         AND r.dest=?
-    ORDER BY
-        af.fare ASC
-) AS fares
-WHERE fares.remain > 0
-LIMIT 15;
+) AS flights
+WHERE flights.remain > 0
+LIMIT 20;
